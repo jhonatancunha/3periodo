e19ef8c199736495e667633403d5d1947f5162d2
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-08-08 19:28:45 -0300

    :art: Tarefa de ED2 finalizada.

--- ESTRUTURA DE DADOS-2/ADNP/SEMANA 1 - ORDENAÇÃO/main.c
+++ ESTRUTURA DE DADOS-2/ADNP/SEMANA 1 - ORDENAÇÃO/main.c
@@ -4,46 +4,46 @@
 #define SEED 0
 
 // PROTOTYPES
-void selectionSort(int *vetor, int size, int *counter, int *trocas);
-void bubbleSort(int *vetor, int size, int *counter, int *trocas);
-void insertionSort(int *vetor, int size, int *counter);
-int max(int *vetor, int start, int end, int *counter);
-void troca(int *vetor, int bigger, int lastPosition, int *trocas);
+void selectionSort(int *vetor, int size, int *ComparationCounter, int *SwapCounter);
+void bubbleSort(int *vetor, int size, int *ComparationCounter, int *SwapCounter);
+void insertionSort(int *vetor, int size, int *ComparationCounter);
+int max(int *vetor, int start, int end, int *ComparationCounter);
+void troca(int *vetor, int bigger, int lastPosition, int *SwapCounter);
 void printVetor(int *vetor, int size);
-void insertion(int *vetor, int size, int *counter);
+void insertion(int *vetor, int size, int *ComparationCounter);
 int* randomVector(int n, int max, int seed);
 
-void insertionSort(int *vetor, int size, int *counter){
+void insertionSort(int *vetor, int size, int *ComparationCounter){
   for(int i = 1; i < size; i++){
-    insertion(vetor, i, counter);
+    insertion(vetor, i, ComparationCounter);
   }
 }
 
-void selectionSort(int *vetor, int size, int *counter, int *trocas){
+void selectionSort(int *vetor, int size, int *ComparationCounter, int *SwapCounter){
   int bigger;
   for(int i = size-1; i > 0; i--){
-      bigger = max(vetor,0, i, counter);
-      troca(vetor, bigger, i, trocas);
+      bigger = max(vetor,0, i, ComparationCounter);
+      troca(vetor, bigger, i, SwapCounter);
   }
 }
 
-void bubbleSort(int *vetor, int size, int *counter , int *trocas){
+void bubbleSort(int *vetor, int size, int *ComparationCounter , int *SwapCounter){
   for(int i = size-1; i > 0; i--){
     for(int j = 0; j < i; j++){
-      *counter += 1;
+      *ComparationCounter += 1;
       if(vetor[j] > vetor[j+1]){
-        troca(vetor, j, j+1, trocas);
+        troca(vetor, j, j+1, SwapCounter);
       }
     }
   }
 }
 
-void insertion(int *vetor, int size, int *counter){
+void insertion(int *vetor, int size, int *ComparationCounter){
   int i = size-1;
   int savedValue = vetor[size];
 
   while(i >= 0 && vetor[i] > savedValue){
-    *counter += 1;
+    *ComparationCounter += 1;
     vetor[i+1] = vetor[i];
     i--;
   }
@@ -51,11 +51,11 @@ void insertion(int *vetor, int size, int *counter){
   vetor[i+1] = savedValue;
 }
 
-int max(int *vetor, int start, int end, int *counter){
+int max(int *vetor, int start, int end, int *ComparationCounter){
   int bigger = end;
 
   for(int i = start; i <= end; i++){
-      *counter += 1;
+      *ComparationCounter += 1;
       if(vetor[i] > vetor[end] && vetor[i] > vetor[bigger]){
         bigger = i;
       }
@@ -65,8 +65,8 @@ int max(int *vetor, int start, int end, int *counter){
   return bigger;
 }
 
-void troca(int *vetor, int bigger, int lastPosition, int *trocas){
-  *trocas += 1;
+void troca(int *vetor, int bigger, int lastPosition, int *SwapCounter){
+  *SwapCounter += 1;
   int n1 = vetor[lastPosition];
   vetor[lastPosition] = vetor[bigger];
   vetor[bigger] = n1;
@@ -88,11 +88,11 @@ int* randomVector(int n, int max, int seed){
 }
 
 int main(int argc, char** argv){
-  int counter = 0;
-  int size = 10000;
+  int ComparationCounter = 0;
+  int size = 10;
   int max = 10000;
 
-  int trocas = 0;
+  int SwapCounter = 0;
 
   clock_t time;
 
@@ -103,36 +103,36 @@ int main(int argc, char** argv){
   printf("=============\nBubble Sort\n");
 
   time = clock();
-  bubbleSort(vet2, size, &counter, &trocas);
+  bubbleSort(vet2, size, &ComparationCounter, &SwapCounter);
   time = clock() - time;
   
 
-  printf("Comparations Bubble Sort: %d - Change Positions: %d\n", counter, trocas);
+  printf("Comparations Bubble Sort: %d \nSwap Counter: %d\n", ComparationCounter, SwapCounter);
   printf("It took %f seconds.\n", ((float)time/CLOCKS_PER_SEC));
-  counter = 0;
-  trocas = 0;
+  ComparationCounter = 0;
+  SwapCounter = 0;
 
   int *vet4 = randomVector(size, max, SEED);
   printf("=============\nInsertion Sort\n");
  
   time = clock();
-  insertionSort(vet4, size, &counter);
+  insertionSort(vet4, size, &ComparationCounter);
   time = clock() - time;
 
-  printf("Comparations Insertion Sort: %d\n", counter);
+  printf("Comparations Insertion Sort: %d\n", ComparationCounter);
   printf("It took %f seconds.\n", ((float)time/CLOCKS_PER_SEC));
-  counter = 0;
+  ComparationCounter = 0;
 
    int *vet = randomVector(size, max, SEED);
   printf("=============\nSelection Sort\n");
 
   time = clock();
-  selectionSort(vet, size, &counter , &trocas);
+  selectionSort(vet, size, &ComparationCounter , &SwapCounter);
   time = clock() - time;
 
-  printf("Comparations Selection Sort: %d - Change Positions: %d\n", counter, trocas);
+  printf("Comparations Selection Sort: %d \nSwap Counter: %d\n", ComparationCounter, SwapCounter);
   printf("It took %f seconds.\n", ((float)time/CLOCKS_PER_SEC));
-  counter = 0;
+  ComparationCounter = 0;
 
   return 0;
 }
