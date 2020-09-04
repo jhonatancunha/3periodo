2e55db976934bb89851d3f9408e39b8a7975e556
Jhonatan Cunha <jhonatancunha@alunos.utfpr.edu.br>
2020-09-04 18:23:58 -0300

    :sparkles: Algoritmo HeapMinK implementado.

--- ESTRUTURA DE DADOS-2/ADNP/SEMANA 5 -BUSCA SEQUENCIAL E BINARIA/main.c
+++ ESTRUTURA DE DADOS-2/ADNP/SEMANA 5 -BUSCA SEQUENCIAL E BINARIA/main.c
@@ -2,11 +2,14 @@
 #include "busca.h"
 
 int main(int argc, char** argv){
-    int v[5] = { 1, 0, 55, 3, 2};
+    int v[5] = { 1, 2, 55, 6, 3};
     // int *v2 = randomVectorUniqueElems(10, 0);
     // int v2[4] = {0, 1, 4, 5};
     // printf("%d", SelectionMinK(v2, 4, 3));
 
-    printf("%d\n", HeapMinK(v, 5, 4));
+    printf("%d\n", HeapMinK(v, 5, 1));
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", v[i]);
+    }
     return 0;
 }
