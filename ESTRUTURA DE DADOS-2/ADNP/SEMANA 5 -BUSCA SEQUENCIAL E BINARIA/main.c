9ab26854e17704790b703806b22315112e8cd749
Jhonatan Cunha <jhonatancunha@alunos.utfpr.edu.br>
2020-09-06 14:03:12 -0300

    :sparkles: Algoritmo de QuickMinK implementado.

--- ESTRUTURA DE DADOS-2/ADNP/SEMANA 5 -BUSCA SEQUENCIAL E BINARIA/main.c
+++ ESTRUTURA DE DADOS-2/ADNP/SEMANA 5 -BUSCA SEQUENCIAL E BINARIA/main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include "busca.h"
 
-int main(int argc, char** argv){
-    int v[5] = { 1, 2, 55, 6, 3};
-    // int *v2 = randomVectorUniqueElems(10, 0);
-    // int v2[4] = {0, 1, 4, 5};
-    // printf("%d", SelectionMinK(v2, 4, 3));
-
-    printf("%d\n", HeapMinK(v, 5, 1));
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", v[i]);
+int
+main (int argc, char **argv)
+{
+  int v[5] = { 3, 1, 26, 2, 55};
+  // int *v2 = randomVectorUniqueElems(10, 0);
+  // int v2[4] = {0, 1, 4, 5};
+  // printf("%d", SelectionMinK(v2, 4, 3));
+  for (int i = 0; i < 5; i++)
+    {
+      printf ("%d ", v[i]);
     }
-    return 0;
+    
+    printf("\n");
+
+
+  printf ("%d\n", QuickMinK (v, 0, 4, 3));
+  for (int i = 0; i < 5; i++)
+    {
+      printf ("%d ", v[i]);
+    }
+  return 0;
 }
