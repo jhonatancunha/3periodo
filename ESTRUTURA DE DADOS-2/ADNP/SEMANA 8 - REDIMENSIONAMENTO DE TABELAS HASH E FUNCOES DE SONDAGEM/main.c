01368a26b2147555ed411cfa26afac6d37a8f5a9
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-09-30 19:07:32 -0300

    :fix: Removendo funções inutilzadas na ADNP 8.

--- ESTRUTURA DE DADOS-2/ADNP/SEMANA 8 - REDIMENSIONAMENTO DE TABELAS HASH E FUNCOES DE SONDAGEM/main.c
+++ ESTRUTURA DE DADOS-2/ADNP/SEMANA 8 - REDIMENSIONAMENTO DE TABELAS HASH E FUNCOES DE SONDAGEM/main.c
@@ -2,19 +2,6 @@
 #include <stdlib.h>
 #include "tabelahash_end_aberto.h"
 
-
-void funcOfTest(int m[], int size, FUNC_HASH tipo_hash){
-  int n = 10000;
-  int max_var = n*10;
-  int seed = 42;
-
-  for(int i = 0; i < size; i++){
-    THEA *t = THEA_CriaRandom(m[i], n, max_var, seed, tipo_hash);
-    printf("M : %d - N: %d - Alpha: %.3f - MAL-SUCEDIDO: %.3f - BEM-SUCEDIDO: %.3f - MAIOR CLUSTER: %d\n\n",
-    t->m, t->n, (float)t->n/t->m, THEA_TamMedioClusters(t), THEA_CustoBemSucedida(t), THEA_ClusterMaximo(t));
-  }
-}
-
 int main(int argc, char** argv){
   int m = 5;
   int n = 100;
--- ESTRUTURA DE DADOS-2/ADNP/SEMANA 8 - REDIMENSIONAMENTO DE TABELAS HASH E FUNCOES DE SONDAGEM/tabelahash_end_aberto.h
+++ ESTRUTURA DE DADOS-2/ADNP/SEMANA 8 - REDIMENSIONAMENTO DE TABELAS HASH E FUNCOES DE SONDAGEM/tabelahash_end_aberto.h
@@ -44,21 +44,7 @@ int THEA_HashDuplo(THEA* TH, int chave, int k);
 // FUNÇÃO DE INSERÇÃO DE VALORES E CHAVES DENTRO DA TABELA HASH
 int THEA_Inserir(THEA* TH, int chave, int valor);
 
-// FUNÇÃO QUE BUSCA DETERMINADA CHAVE NA TABELA HASH
-int THEA_Busca(THEA* TH, int chave);
 
-// FUNÇÃO QUE REMOVE UM ELEMENTO DADO UM DETERMINADA CHAVE
-// REMOVER É O MESMO QUE COLOCAR O ELEMENTO COMO APAGADO
-void THEA_Remover(THEA* TH, int chave);
-
-// IMPRIME TODA A TABELA HASH DE ENDERAÇAMENTO ABERTO
-void THEA_Print(THEA *TH);
-
-int THEA_ClusterMaximo(THEA* TH);
-float THEA_TamMedioClusters(THEA* TH);
-float THEA_CustoBemSucedida(THEA *TH);
 int* random_vector(int n, int max, int seed);
 THEA* THEA_CriaRandom(int m, int n, int max, int seed, FUNC_HASH tipo_hash);
 
-int min(THEA* TH);
-int max(THEA* TH);
